4954fe042ad4d26e4b007d06c07dc6e508c90732
Joshua Kennedy <josh@outthinkgroup.com>
2021-06-28 14:09:52 -0400

    basic tic tac toe

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,7 +1,116 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import "./styles.css"
+function emptyBoard(size){
+  const board = []
+  for(let y = 0; y < size; y++){
+    board.push([])
+    for(let x = 0; x < size; x++){
+      board[y].push("")
+    }
+  }
+  return board
+}
 
-function TicTacToe(){
-  return <h1>TICTACTOE</h1>
+const players = ['x', 'o'];
+
+function allEqual(...args){
+  return args.every(v=>{
+    return v!==""&&v===args[0]
+  })
+}
+
+function getAvailableTiles(board){
+  return board.reduce((available, row, y)=>{
+    row.forEach((tile,x)=>{
+      if(tile===""){
+        available.push([x,y])
+      }
+    })
+    return available
+  },[])
+}
+
+function checkWinner(board){
+
+  //check horizontals
+  for(let row = 0; row<board.length; row++){
+    if(allEqual(...board[row])){
+      return board[row][0]
+    }
+  }
+
+  //check verticals
+  for(let column = 0; column<board.length; column++){
+    console.log([board[0][column], board[1][column], board[2][column]])
+    if(allEqual(board[0][column], board[1][column], board[2][column])){
+      return board[0][column]
+    }
+  }
+  
+
+  //TODO refactor to make dynamic size board
+  // check diagonals
+  if(allEqual(board[0][0], board[1][1], board[2][2])){
+    return board[0][0]
+  }
+  if(allEqual(board[0][2], board[1][1], board[2][0])){
+    return board[0][2]
+  }
+
+  //Check for Tie
+
+  if(getAvailableTiles(board).length===0){
+    return 'CAT'
+  }
+
+}
+
+function TicTacToe({}){
+  const [board, setBoard] = useState(()=>emptyBoard(3))
+  const [currentPlayer, setCurrentPlayer] = useState(null)
+  const [gameWinner, setGameWinner] = useState(null)
+  function handlePlaceMark({x,y}){
+    setBoard(prev=>{
+      const newBoard = [...prev]
+      newBoard[y][x] = currentPlayer
+      return newBoard
+    })
+    
+  }
+
+  useEffect(()=>{
+    console.log(board)
+    const gameResult = checkWinner(board)
+    if(!gameResult && board){
+      setCurrentPlayer((prev)=>{
+        const currentPlayerIndex = players.findIndex(p=>p==prev)
+        const nextPlayer = players[(currentPlayerIndex + 1) % (players.length)]
+        
+        return nextPlayer
+      })
+    } else{
+      const winner = gameResult == 'CAT' ? 'TIE' : gameResult;
+      setGameWinner(winner);
+    }
+      
+  }, [board, setCurrentPlayer, checkWinner])
+
+
+  return <div className="board-wrapper">
+      {gameWinner && <h2>The Winner is: <span>{gameWinner}</span></h2>}
+
+      {currentPlayer && <p>Current Player: <span>{currentPlayer=='x'?'Player 1 (X)':'Player 2 (O)'}</span></p>}
+
+      <div className="board">
+      {board.map((row,y)=>{
+        return row.map((tile,x)=>{
+          return <div className="tile" key={`${x},${y}`}>
+            <button onClick={()=>handlePlaceMark({x,y})}>{tile}</button>
+          </div>
+        })
+      })}
+    </div>
+  </div>
 }
 
 export default TicTacToe
