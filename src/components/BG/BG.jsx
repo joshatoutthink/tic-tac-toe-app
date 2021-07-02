4719226a587d942777fd7f8ecfc9119c5024736a
Joshua Kennedy <josh@outthinkgroup.com>
2021-07-02 10:17:46 -0400

    static bg

--- src/components/BG/BG.jsx
+++ src/components/BG/BG.jsx
@@ -11,6 +11,7 @@ function BG() {
   return (
     <div className="bg">
       <svg
+        id="#svg-bg"
         viewBox={`0 0 ${windowWidth} ${windowHeight}`}
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
@@ -18,27 +19,37 @@ function BG() {
         <g clipPath="url(#clip0)">
           <rect width={windowWidth} height={windowHeight} fill="#6100DC" />
           <g filter="url(#filter0_f)">
-            <circle cx="1145" cy="262" r="346" fill="#CA00DC" />
-          </g>
-          <g filter="url(#filter2_f)">
-            <circle cx="446" cy="740" r="346" fill="#FBDD8F" />
-          </g>
-          <g filter="url(#filter1_f)">
             <circle
-              cx={wRatio * 25}
-              cy={hRatio * 262}
-              r={346 / (wRatio * hRatio)}
-              fill="#13EDFB"
+              cx={1145 / wRatio}
+              cy={262 / hRatio}
+              r={600 / (wRatio * hRatio)}
+              fill="#CA00DC"
+            />
+          </g>
+          <g filter="url(#filter0_f)">
+            <circle
+              cx={wRatio * 446}
+              cy={hRatio * 740}
+              r={900 / (wRatio * hRatio)}
+              fill="hsl(43 93% 85%)"
+            />
+          </g>
+          <g filter="url(#filter0_f)">
+            <circle
+              cx={25 / wRatio}
+              cy={262 / hRatio}
+              r={700 / (wRatio * hRatio)}
+              fill="#13EDFBbb"
             />
           </g>
         </g>
         <defs>
           <filter
             id="filter0_f"
-            x={wRatio * 549}
-            y={hRatio * -334}
-            width={hRatio * 1192}
-            height={hRatio * 1192}
+            x={0 - windowWidth / 4}
+            y={0 - windowHeight / 4}
+            width={windowWidth + windowWidth / 2}
+            height={windowHeight + windowHeight / 2}
             filterUnits="userSpaceOnUse"
             colorInterpolationFilters="sRGB"
           >
@@ -50,52 +61,11 @@ function BG() {
               result="shape"
             />
             <feGaussianBlur
-              stdDeviation="125"
+              stdDeviation="200"
               result="effect1_foregroundBlur"
             />
           </filter>
-          <filter
-            id="filter2_f"
-            x={wRatio * -500}
-            y={hRatio * -206}
-            width={hRatio * 1892}
-            height={hRatio * 1892}
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="BackgroundImageFix"
-              result="shape"
-            />
-            <feGaussianBlur
-              stdDeviation="300"
-              result="effect1_foregroundBlur"
-            />
-          </filter>
-          <filter
-            id="filter1_f"
-            x={wRatio * -921}
-            y={hRatio * -684}
-            width={hRatio * 1892}
-            height={hRatio * 1892}
-            filterUnits="userSpaceOnUse"
-            colorInterpolationFilters="sRGB"
-          >
-            <feFlood floodOpacity="0" result="BackgroundImageFix" />
-            <feBlend
-              mode="normal"
-              in="SourceGraphic"
-              in2="BackgroundImageFix"
-              result="shape"
-            />
-            <feGaussianBlur
-              stdDeviation="300"
-              result="effect1_foregroundBlur"
-            />
-          </filter>
+
           <clipPath id="clip0">
             <rect width={windowWidth} height={windowHeight} fill="white" />
           </clipPath>
